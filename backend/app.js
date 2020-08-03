5211b6ddd3377a1df3bafff67a3cf0bc02652e6a
fb <frantabendik@gmail.com>
2020-08-03 17:10:24 +0200

    another one

--- backend/app.js
+++ backend/app.js
@@ -87,6 +87,10 @@ app.post('/api/suggestion/:id', async (req, res) => {
     res.json(newSignature);
 });
 
+app.get('/', (req, res) =>
+  res.sendFile(path.resolve('..', 'client', 'build', 'index.html'))
+)
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
     app.get('*', (req, res) => {
